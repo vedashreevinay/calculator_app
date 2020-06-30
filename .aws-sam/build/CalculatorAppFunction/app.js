f0b5a6c886bb49cfd1fa345742ad3738204b45e3
Raman <vedashrr@3c22fb3cbb9a.ant.amazon.com>
2020-06-30 12:16:13 -0400

    first commit

---
+++ .aws-sam/build/CalculatorAppFunction/app.js
@@ -0,0 +1,19 @@
+
+async function add(x,y){
+    //await new Promise(resolve => setTimeout(resolve, 10000));
+    return (x+y);
+}
+
+exports.handler = async (event) => {
+    console.log(event);
+    var x = event['pathParameters']['x']
+    var y = event['pathParameters']['y']
+    
+    var z = await add(Number(x), Number(y));
+    console.log(z)
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(z),
+    };
+    return response;
+};
